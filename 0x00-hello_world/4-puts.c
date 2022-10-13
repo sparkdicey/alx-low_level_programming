7e254af0465a542bd74f33dd27d014cb2a3c2ea6
Godwin <sparkdicey@gmail.com>
2022-10-13 05:33:50 -0700

    test4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+/**
+ * main - function
+ * Return: always 0
+ */
 int main(void)
-	{
+{
 
-	 puts("Programming is like building a multilingual puzzle\n")
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
 
-	}
+}

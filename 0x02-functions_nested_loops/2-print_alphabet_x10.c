62c9ac106340b166bcc045fdbf6feacb2763691e
Godwin <sparkdicey@gmail.com>
2022-10-18 16:51:02 -0700

    test3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - prints the alphabet in lowercase
+ * print_alphabet_x10 - prints alphabet 10 times
  */
 
-void print_alphabet (void)
+void print_alphabet_x10(void)
 {
+	int ten;
 
 	char la;
 
+	for (ten = 0; ten <= 9; ten++)
+{
 	for (la = 'a'; la <= 'z'; la++)
-		_putchar (la) ;
+		_putchar(la);
 
 	_putchar('\n');
+}
 }
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main,h"
+
+/**
+ * _islower - checks for lowercase
+ * @c: the character
+ * Return: 1 if lowercase or else 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= '2')
+		return (1);
+	else
+		return (0);
+}
